20f0da6033a5c2e6c25a966d311ecc12a9310c77
Camille8960 <128013149+Camille8960@users.noreply.github.com>
2026-06-15 17:15:28 +0800

    Update sw.js

--- calendar/sw.js
+++ calendar/sw.js
@@ -1,24 +1,17 @@
-const CACHE = 'mycal-v1';
-const FILES = ['./'];
+const CACHE = 'mycal-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-    self.skipWaiting();
-    });
+  self.skipWaiting();
+});
 
-    self.addEventListener('activate', e => {
-      e.waitUntil(caches.keys().then(keys =>
-          Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-            ));
-              self.clients.claim();
-              });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
 
-              self.addEventListener('fetch', e => {
-                e.respondWith(
-                    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-                          const clone = res.clone();
-                                caches.open(CACHE).then(c => c.put(e.request, clone));
-                                      return res;
-                                          }))
-                                            );
-                                            });
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
